fcad1b6da21fef58ad513b1138b21c2ce2b392d7
unknown <bhaskarnavuluri9@gmail.com>
2021-01-20 14:30:10 +0530

    added doublet Combination

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -29,3 +29,49 @@
 	    echo "percentage of $combination is ${singlet[$combination]}%"
 	done
 
+echo "DOBLET COMBINATION"
+
+declare -A doublet=( 
+
+                                [HH]=0
+                                [HT]=0
+                                [TH]=0
+                                [TT]=0
+
+                    )
+
+for(( i = 1; i <= flips; i++ ))
+do
+    echo -n "Flip-$i is "
+    (( coin1 = RANDOM%2 ))
+    (( coin2 = RANDOM%2 ))
+
+    #0 heads
+    #1 tails
+    case $coin1$coin2 in 
+        00)
+            echo "Head Head"
+            (( doublet[HH]++ )) 
+            ;;
+        01)
+            echo "Head Tail"
+            (( doublet[HT]++ ))
+            ;;
+        10)
+            echo "Tail Head"
+            (( doublet[TH]++ ))
+            ;;
+        11)
+            echo "Tail Tail"
+            (( doublet[TT]++ ))
+            ;;
+    esac
+done
+
+for combination in ${!doublet[@]}
+do
+    percentage=$(( ${doublet[$combination]}  * 100 / flips ))
+    doublet[$combination]=$percentage
+    echo "percentage of $combination is ${doublet[$combination]}%"   
+done
+
