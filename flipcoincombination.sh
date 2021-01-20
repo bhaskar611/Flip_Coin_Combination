c812b227b60b2e273da96dbc5b2b9a5763c5b463
unknown <bhaskarnavuluri9@gmail.com>
2021-01-20 14:56:09 +0530

    added TripletCombination

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -75,3 +75,68 @@ do
     echo "percentage of $combination is ${doublet[$combination]}%"   
 done
 
+echo "TripletCombination"
+
+
+declare -A triplet=( 
+                      [HHH]=0
+                      [HHT]=0
+                      [HTH]=0
+                      [HTT]=0
+                      [THH]=0
+                      [THT]=0
+                      [TTH]=0
+                      [TTT]=0
+
+                             )
+for((i = 1; i <= flips; i++ ))
+do
+    echo -n "Flip-$i is "
+    (( coin1 = RANDOM%2 ))
+    (( coin2 = RANDOM%2 ))
+    (( coin3 = RANDOM%2 ))
+
+    #0 for heads
+    #1 for tails
+    case $coin1$coin2$coin3 in 
+        000)
+            echo "Head Head Head"
+            (( triplet[HHH]++ )) 
+            ;;
+        001)
+            echo "Head Head Tail"
+            (( triplet[HHT]++ ))
+            ;;
+        010)
+            echo "Head Tail Head"
+            (( triplet[HTH]++ ))
+            ;;
+        011)
+            echo "Head Tail Tail"
+            (( triplet[HTT]++ ))
+            ;;
+        100)
+            echo "Tail Head Head"
+            (( triplet[THH]++ )) 
+            ;;
+        101)
+            echo "Tail Head Tail"
+            (( triplet[THT]++ ))
+            ;;
+        110)
+            echo "Tail Tail Head"
+            (( triplet[TTH]++ ))
+            ;;
+        111)
+           echo "Tail Tail Tail"
+            (( triplet[TTT]++ ))
+            ;;
+    esac
+done
+for combination in ${!triplet[@]}
+do
+    percentage=$(( ${triplet[$combination]}  * 100 / flips ))
+    triplet[$combination]=$percentage
+    echo "percentage of $combination is ${triplet[$combination]}%"
+done
+
